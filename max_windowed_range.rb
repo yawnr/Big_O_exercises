7503da706629c01a57031ecbc7820878cc42e019
App Academy Student <appacademy@Apps-Mac-mini-21.local>
2015-10-08 17:37:38 -0400

    Add window_max_range method using Queues.

--- max_windowed_range.rb
+++ max_windowed_range.rb
@@ -1,4 +1,4 @@
-def windowed_max_range(array, window_size)
+def bad_windowed_max_range(array, window_size)
   i = 0
   current_max_range = 0
 
@@ -17,14 +17,14 @@ end
 # O(n ** 2)
 
 
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+# p bad_windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+# p bad_windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+# p bad_windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+# p bad_windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
 
 class MyStack
 
-attr_reader :min, :max
+attr_reader :min, :max, :store
 
   def initialize
     @store = []
@@ -49,11 +49,11 @@ attr_reader :min, :max
   end
 
   def max
-    @max_list.peek
+    @max_list[-1]
   end
 
   def min
-    @min_list.peek
+    @min_list[-1]
   end
 
   def peek
@@ -70,7 +70,40 @@ attr_reader :min, :max
 
 end
 
-class StackQueue
+# Checking if our MyStack works
+# a = MyStack.new
+# p a.store
+# puts
+# a.push(3)
+# p a.store
+# p a.max
+# p a.min
+# puts
+# a.push(2)
+# p a.store
+# p a.max
+# p a.min
+# puts
+# a.push(6)
+# p a.store
+# p a.max
+# p a.min
+# puts
+# a.push(-1)
+# p a.store
+# p a.max
+# p a.min
+# puts
+# a.pop
+# p a.store
+# p a.max
+# p a.min
+
+
+
+class StackQueue < MyStack
+
+  attr_accessor :queue
 
  def initialize
    @queue = MyStack.new
@@ -96,6 +129,14 @@ class StackQueue
   first_el
  end
 
+ def max
+   @queue.max
+ end
+
+ def min
+   @queue.min
+ end
+
  def size
    size = 0
    @queue.each do |el|
@@ -110,13 +151,63 @@ class StackQueue
 
 end
 
+class MinMaxStackQueue < StackQueue
 
-a = StackQueue.new
-a.enqueue(1)
-a.enqueue(2)
-a.enqueue(3)
-p a
+  def initialize
+    super
+  end
 
-a.dequeue
-a.enqueue(5)
-p a
+  def max
+    @queue.max
+  end
+
+  def min
+    @queue.min
+  end
+
+end
+
+#Checking if our StackQueue works
+# a = MinMaxStackQueue.new
+# a.enqueue(1)
+# a.enqueue(7)
+# a.enqueue(3)
+# puts a.max
+# puts a.min
+# puts
+#
+# puts "then we dequeue"
+#
+# a.dequeue
+# puts a.max
+# puts a.min
+# puts
+#
+# puts "then we enqueue 5"
+# a.enqueue(5)
+# puts a.max
+# puts a.min
+
+def windowed_max_range(array, window_size)
+  queue = MinMaxStackQueue.new
+
+  array[0...window_size].each do |el|
+    queue.enqueue(el)
+  end
+
+  current_max_range = queue.max - queue.min
+
+  (window_size...array.length).each do |i|
+    queue.enqueue(array[i])
+    queue.dequeue
+
+    current_max_range = queue.max - queue.min if current_max_range < queue.max - queue.min
+  end
+
+  current_max_range
+end
+
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
